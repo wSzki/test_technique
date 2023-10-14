6428f49ff6a965dd1ca9b29878d926c572b2e6ee
wsz <o9b9qzaa@wsz.anonaddy.com>
2023-10-14 09:48:49 +0200

    moved popup loaction for mobile when keyboard is on

--- app/next/src/components/PopupNoSearchResult.tsx
+++ app/next/src/components/PopupNoSearchResult.tsx
@@ -7,7 +7,7 @@ export default function PopupNoSearchResult ({children}:any) {
 
 	return (
 		<>
-			<div className = {`${search_result_found?"hidden":"flex"} bg-yellow-500 p-4 rounded absolute bottom-[75px] left-1/2 transform -translate-x-1/2 -translate-y-1/2`}>
+			<div className = {`${search_result_found?"hidden":"flex"} bg-yellow-500 p-4 rounded absolute justify-center top-[42vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2`}>
 				No search result
 			</div>
 		</>
